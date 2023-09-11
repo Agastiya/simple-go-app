54321e735afde2d09ed4d84a50d72050388d57d1
T.Putra  Agastiya <ageztya.putra@gmail.com>
2023-09-11 14:54:24 +0700

    add simple calculator app

---
+++ calculator.go
@@ -0,0 +1,17 @@
+package main
+
+func addition(a int, b int) int {
+	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func division(a int, b int) int {
+	return a / b
+}
